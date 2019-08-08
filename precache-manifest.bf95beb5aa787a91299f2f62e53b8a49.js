14884fc2c439c20f66129e59e949701e61157e58
Bignoux Didier <didieransleybignoux@live.com>
2019-08-08 11:24:11 +0400

    Updates

---
+++ precache-manifest.bf95beb5aa787a91299f2f62e53b8a49.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f9059b67ba8399cbde1",
+    "url": "/react-auth-app/static/js/runtime~main.3f9059b6.js"
+  },
+  {
+    "revision": "7891856ea516cbe4602b",
+    "url": "/react-auth-app/static/js/main.7891856e.chunk.js"
+  },
+  {
+    "revision": "ff74199c59b1e60be9e1",
+    "url": "/react-auth-app/static/js/1.ff74199c.chunk.js"
+  },
+  {
+    "revision": "ff74199c59b1e60be9e1",
+    "url": "/react-auth-app/static/css/1.69e11098.chunk.css"
+  },
+  {
+    "revision": "88d9aa9ca33c63c5e306ad740d5062a1",
+    "url": "/react-auth-app/index.html"
+  }
+];
